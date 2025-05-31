adfb47ba3cd962c594e7e92a1db79088ceffe48b
MD Leon Islam <80670800+MdLeon-official@users.noreply.github.com>
2025-05-31 22:42:32 +0600

    Create 1.Basic_Structure.java

---
+++ Learning_Java/1.Basic_Structure.java
@@ -0,0 +1,26 @@
+public class S1basicstructure {
+
+    public static void main(String[] args) {
+        //This is my first java programm
+
+        /*
+        This is
+        a multiline comment
+        */
+
+        System.out.println("Hello World");
+        System.out.println("ONE PIECE does exist");
+        System.out.println(); //sout - for shortcut
+    }
+}
+/*
+| Element                | Description                                           |
+| ---------------------- | ----------------------------------------------------- |
+| `class`                | Blueprint for objects; contains methods and variables |
+| `main()`               | Starting point of any Java application                |
+| `public`               | Access modifier (visible everywhere)                  |
+| `static`               | No need to create object                              |
+| `void`                 | No return value                                       |
+| `String[] args`        | Command-line arguments                                |
+| `System.out.println()` | Print to console                                      |
+*/
