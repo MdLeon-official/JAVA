600a640c7f6a71d7aaef876f29bc4ed7d6f28795
MD Leon Islam <80670800+MdLeon-official@users.noreply.github.com>
2025-06-02 12:05:19 +0600

    Create 4. Arithmetic.java

---
+++ Learning_Java/4. Arithmetic.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 2;
+        int z;
+        
+        // Arithmetic operators
+        z = x + y;
+        // z = x - y;
+        // z = x * y;
+        // z = x + y;
+        // z = x % y;
+
+        // Augmented Assignment Operators
+        // x += y;
+        // x -= y;
+        // x *= y;
+        // x /= y;
+        // x %= y;
+
+        // Incerament and Decrement Operators
+        int i =1;
+        i++;
+        i--;
+
+        // Order of Operations: P-E-M-D-A-S
+        double result = 3 + 4 * (7-5) / 2.0;
+
+        System.out.println(z);
+        System.out.println(i);
+        System.out.println(result);
+    }
+}
