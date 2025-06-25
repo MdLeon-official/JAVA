f61a4736931846a5bea1488e57ff15f7195572e7
MD Leon Islam <80670800+MdLeon-official@users.noreply.github.com>
2025-06-26 01:24:00 +0600

    Create 16. Face Printer.java

---
+++ w3resources/Basic part-I/16. Face Printer.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf(" +\"\"\"\"\"+\n");
+        System.out.printf("[| o o |]\n");
+        System.out.printf(" |  ^  |\n");
+        System.out.printf(" | '-' |\n");
+        System.out.printf(" +-----+\n");
+    }
+}
