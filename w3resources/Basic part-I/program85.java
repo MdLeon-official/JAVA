acbf564a19827db3c8ede1748e89218f8a7a27b9
MD Leon Islam <80670800+MdLeon-official@users.noreply.github.com>
2025-06-30 13:15:00 +0600

    Create program85.java

---
+++ w3resources/Basic part-I/program85.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class program85 {
+
+    public static void main(String[] args) {
+        String string1 = "Hello how are you?";
+
+        boolean startsWithHello = string1.startsWith("Hello");
+
+        System.out.println(startsWithHello);
+    }
+}
