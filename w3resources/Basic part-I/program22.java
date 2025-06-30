b3db1f94315c8bc53d35555c07d0f60be193cd15
MD Leon Islam <80670800+MdLeon-official@users.noreply.github.com>
2025-06-30 12:41:24 +0600

    Create program22.java

---
+++ w3resources/Basic part-I/program22.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class program22 {
+
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.print("Enter a binary number: ");
+            String binary = input.nextLine();
+            int decimal = Integer.parseInt(binary, 2);
+            System.out.println("Decimal: " + decimal);
+        }
+    }
+}
