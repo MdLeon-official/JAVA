51c0a02ea9323300d1dc3703c90c7390a7c2e61d
MD Leon Islam <80670800+MdLeon-official@users.noreply.github.com>
2025-06-30 13:27:03 +0600

    Create program89.java

---
+++ w3resources/Basic part-I/program89.java
@@ -0,0 +1,10 @@
+public class program89 {                                                                            
+    @SuppressWarnings("removal")                                                              
+    public static void main(String[] args) {                                                   
+        // Print a message indicating the display of the system security interface             
+        System.out.println("System security interface:");                                      
+                                                                                               
+        // Retrieve and print the system security manager using System.getSecurityManager()    
+        System.out.println(System.getSecurityManager());                                       
+    }                                                                                          
+}
