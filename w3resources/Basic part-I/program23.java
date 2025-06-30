a2f2c4f3f6eca76bcea39e3de745250562b7a58b
MD Leon Islam <80670800+MdLeon-official@users.noreply.github.com>
2025-06-30 12:38:45 +0600

    Create program23.java

---
+++ w3resources/Basic part-I/program23.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class program23 {
+
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.print("Enter a binary number: ");
+            String binary = input.nextLine();
+            int decimal = Integer.parseInt(binary, 2);
+            String hex = Integer.toHexString(decimal).toUpperCase();
+            System.out.println("Octal: " + hex);
+        }
+    }
+}
